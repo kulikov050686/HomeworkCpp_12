75981c65268709b952c54c2e263d4bb39eebf7ee
kulikov050686 <62894184+kulikov050686@users.noreply.github.com>
2022-04-12 14:12:02 +0300

    Внесены изменения в проект

--- SnakeGameLib/SnakeGame.cpp
+++ SnakeGameLib/SnakeGame.cpp
@@ -8,46 +8,6 @@ namespace SnakeGameLib
 		system("cls");
 		setlocale(LC_ALL, "Russian.utf8");
 
-        if (!glfwInit())
-        {
-            std::cout << "Failed to initialize GLFW!!!" << std::endl;
-            return;
-        }
-
-        GLFWwindow* window = glfwCreateWindow(1200, 1200, "Snake", nullptr, nullptr);
-
-        if (!window)
-        {
-            std::cout << "Window failed!!!" << std::endl;
-            glfwTerminate();
-            return;
-        }
-
-        glfwMakeContextCurrent(window);
-        
-        std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
-        std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
-
-        if (glewInit() != GLEW_OK)
-        {
-            std::cout << "Failed to initialize GLEW!!!" << std::endl;
-            return;
-        }
-
-        while (!glfwWindowShouldClose(window))
-        {
-
-            glClear(GL_COLOR_BUFFER_BIT);
-
-
-            glfwSwapBuffers(window);
-
-
-            glfwPollEvents();
-        }
-
-        glfwTerminate();
-
 		/*auto controllerLocator = std::make_shared<ControllerLocator>();
 		auto game = std::make_shared<Game>(controllerLocator);
 
--- SnakeGameLib/SnakeGame.h
+++ SnakeGameLib/SnakeGame.h
@@ -1,6 +1,4 @@
 #pragma once
-#include "GLEW/include/glew.h"
-#include "GL/include/glfw3.h"
 #include <iostream>
 #include <memory>
 #include "ControllerLocator.h"
